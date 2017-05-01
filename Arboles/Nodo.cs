f20d9df59778ebfd931c1112475b4e2544cefadb
RationalKuma4 <ans.alv.46gmail.com>
2017-05-01 10:57:34 -0500

    Commit inicial

---
+++ Arboles/Nodo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Arboles
+{
+    public class Nodo
+    {
+        public Nodo(int valor, Nodo nodoIzquiero, Nodo nodoDerecho)
+        {
+            Valor = valor;
+            NodoIzquierdo = nodoIzquiero;
+            NodoDerecho = nodoDerecho;
+        }
+
+        public int Valor { get; set; }
+        public Nodo NodoDerecho { get; set; }
+        public Nodo NodoIzquierdo { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nodo">Nodo el cual imprimiremos recusivamente in-order</param>
+        public static void Traversal(Nodo nodo)
+        {
+            MakeTraversal(nodo.NodoIzquierdo);
+            Console.Out.Write($"{nodo.Valor}-");
+            MakeTraversal(nodo.NodoDerecho);
+        }
+
+        private static bool IsNodeNull(Nodo nodo)
+        {
+            return nodo == null;
+        }
+
+        private static void MakeTraversal(Nodo nodo)
+        {
+            if (!IsNodeNull(nodo))
+                Traversal(nodo);
+        }
+    }
+}
---
+++ Arboles/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Arboles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var nodoCinco = new Nodo(9, null, null);
+            var nodoSeis = new Nodo(14, null, null);
+            var nodoSiete = new Nodo(19, null, null);
+            var nodoCuatro = new Nodo(23, nodoSiete, null);
+            var nodoDiez = new Nodo(67, null, null);
+            var nodoOcho = new Nodo(54, null, nodoDiez);
+            var nodoNueve = new Nodo(76, null, null);
+
+            var nodoTres = new Nodo(12, nodoCinco, nodoSeis);
+            var nodoDos = new Nodo(72, nodoOcho, nodoNueve);
+            var nodoUno = new Nodo(17, nodoTres, nodoCuatro);
+
+            var raiz = new Nodo(50, nodoUno, nodoDos);
+            Nodo.Traversal(raiz);
+
+            Console.In.ReadLine();
+        }
+    }
+}
